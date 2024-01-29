c0cc76f8aac9f199fefaa31df54530755435134b
anaseem80 <anaseemamin@gmail.com>
2024-01-29 22:50:13 +0200

    Finshing 65% of project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.html"],
+  content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: "#888883"
+        }
+      },
+      fontFamily:{
+        "honk": 'Honk',
+        "poppins": 'Poppins',
+      }
+    },
   },
   plugins: [],
 }
